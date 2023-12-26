dfdffdccc138dce6fa8745909c39f0dd30f148e4
Hannael-mich <thanna.lkuz@gmail.com>
2023-12-25 18:49:35 -0600

    Se agrego el cliente para la api de personas

---
+++ SolClientePersonas/WebClientePersonas/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using WebClientePersonas.ServiceReference1;
+
+namespace WebClientePersonas.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            try
+            {
+                using (WebServicePersonasSoapClient ws = new WebServicePersonasSoapClient())
+                {
+                    EntRespuesta r = ws.Obtener();
+                    if (r.Error)
+                    {
+                        throw new Exception(r.Mensaje);
+                    }
+                    List<persona> ls = r.ListaPersonas.ToList();
+                    return View(ls);
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+                return View(new List<persona>());
+            }
+            
+                
+        }
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+        public ActionResult AgregarBD(persona p)
+        {
+            try
+            {
+                using (WebServicePersonasSoapClient ws = new WebServicePersonasSoapClient())
+                {
+                    EntRespuesta r = ws.Agregar(p);
+                    if (r.Error)
+                    {
+                        throw new Exception(r.Mensaje);
+                    }
+                    TempData["ok"] = "Se agrego el registro";
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = ex.Message;
+                return View("Agregar");
+            }
+        }
+    }
+}
